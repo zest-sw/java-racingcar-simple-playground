82d5d38cfce5032f1f14a262c57a846b76af1359
TheDeveloperKwon <akongwon@gmail.com>
2026-03-25 13:42:43 +0900

    test: 우승 자동차 구하기 구현 테스트

---
+++ src/test/java/contest/contestTest.java
@@ -0,0 +1,61 @@
+package contest;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class contestTest {
+    @DisplayName("n대의 자동차가 참여할 수 있다.")
+    @Test
+    void n_대의_자동차가_참여(){
+        //given
+        String[] names = {"car1", "car2", "car3"};
+        int attempt = 5;
+        Contest contest = new Contest(names, attempt);
+
+        //when
+        String[] actual = contest.getParticipantsName();
+
+        //then
+        assertThat(actual).isEqualTo(names);
+
+    }
+
+    @DisplayName("자동차 경주 게임을 완료한 후 누가 우승했는지를 구할 수 있다.")
+    @Test
+    void 우승자를_알_수_있다(){
+        //given
+        String[] names = {"car1", "car2", "car3"};
+        int attempt = 5;
+        Contest contest = new Contest(names, attempt);
+
+        //when
+        contest.startContest();
+        List<String> actual = contest.getWinners();
+
+        //then
+        assertThat(actual).containsAnyOf("car1", "car2", "car3");
+    }
+
+
+    @DisplayName("우승자는 한 명 이상일 수 있다.")
+    @Test
+    void 중복_우승자가_있을_수_있다(){
+        //given
+        String[] names = {"car1", "car2", "car3"};
+        int attempt = 5;
+        Contest contest = new Contest(names, attempt);
+
+        //when
+        contest.startContest();
+        List<String> actual = contest.getWinners();
+
+        //then
+        assertThat(actual).hasSizeGreaterThan(0);
+    }
+
+}
