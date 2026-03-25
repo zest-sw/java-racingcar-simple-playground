4ceaa29940f5c3e48ac4e6fd7a994b9fe307fe43
TheDeveloperKwon <akongwon@gmail.com>
2026-03-25 11:11:07 +0900

    feat: 1단계 자동차 작동 테스트 구현

---
+++ src/test/java/car/CarTest.java
@@ -0,0 +1,101 @@
+package car;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @DisplayName("자동차는 이름을 가지고 있다.")
+    @Test
+    void testCarName() {
+        //given
+        String name = "car1";
+        Car car = new Car(name);
+
+        //when
+        String CarName = car.getName();
+
+        //then
+        assertThat(CarName).isEqualTo(name);
+
+    }
+
+    @DisplayName("자동차는 움직일 수 있다.")
+    @Test
+    void testCarMoving() {
+        //given
+        String name = "car1";
+        Car car = new Car(name);
+
+        //when
+        car.moveCar(4);
+        int position = car.getPosition();
+
+        //then
+        assertThat(position).isEqualTo(1);
+    }
+
+    @DisplayName("자동차는 여러번 움직일 수 있다.")
+    @Test
+    void testCarMovingLot() {
+        //given
+        String name = "car1";
+        Car car = new Car(name);
+
+        //when
+        car.moveCar(4);
+        car.moveCar(3);
+        car.moveCar(8);
+        car.moveCar(6);
+        int position = car.getPosition();
+
+        //then
+        assertThat(position).isEqualTo(3);
+    }
+
+    @DisplayName("자동차는 0~9 사이 랜덤한 값으로 움직인다.(0미만)")
+    @Test
+    void testCarMovingRangeDown() {
+        //given
+        String name = "car1";
+        Car car = new Car(name);
+
+        //then
+        assertThatThrownBy(() -> {
+            car.moveCar(-1);
+        })
+                .hasMessageContaining("자동차 이동 인자가 0보다 작습니다.");
+    }
+
+    @DisplayName("자동차는 0~9 사이 랜덤한 값으로 움직인다.(9초과)")
+    @Test
+    void testCarMovingRangeUp() {
+        //given
+        String name = "car1";
+        Car car = new Car(name);
+
+        //then
+        assertThatThrownBy(() -> {
+            car.moveCar(10);
+        })
+                .hasMessageContaining("자동차 이동 인자가 9보다 큽니다.");
+    }
+
+    @DisplayName("자동차는 0~9 사이 랜덤한 값으로 움직인다.")
+    @Test
+    void testCarMovingRandomly() {
+        //given
+        String name = "car1";
+        Car car = new Car(name);
+
+        //when
+        for (int i = 0; i <= 10000; i++) {
+            car.moveCarRandomly();
+        }
+        int position = car.getPosition();
+
+        //then
+        assertThat(position).isBetween(4000,6000);
+    }
+}
