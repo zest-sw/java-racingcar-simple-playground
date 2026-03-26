56215f4a9f44d03719ea44e3d3174c29b4c95301
TheDeveloperKwon <akongwon@gmail.com>
2026-03-26 11:00:04 +0900

    feat: 3단계 어플리케이션 인터페이스 추가

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,49 @@
+package application;
+
+import contest.Contest;
+
+import java.util.Scanner;
+
+public class Application {
+
+
+    private static String[] parseParticipant(String names) {
+
+        return names.split("\\s*,\\s*");
+    }
+
+    private static void validateCarName(String name) {
+        if (name.length() > 5) {
+            throw new RuntimeException("자동차 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String names = scanner.nextLine();
+        String[] participants = parseParticipant(names);
+        if (participants.length == 0) {
+            throw new RuntimeException("자동차 이름이 잘못 입력되었습니다.(입력값이 없습니다.)");
+        }
+        for (String participant : participants) {
+            validateCarName(participant);
+        }
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int attempt = scanner.nextInt();
+        System.out.println();
+
+
+        System.out.println("실행 결과");
+        Contest contest = new Contest(participants, attempt);
+        contest.startContest();
+
+        String[] winners = contest.getWinners();
+        System.out.println(String.join(", ", winners) + "가 최종 우승했습니다.");
+
+
+    }
+}
