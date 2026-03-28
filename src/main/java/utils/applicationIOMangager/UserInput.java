0f26f34902930228b744809bcf48f999d966554d
TheDeveloperKwon <akongwon@gmail.com>
2026-03-28 19:56:38 +0900

    refactor: userInput 생성자 추가

--- src/main/java/utils/applicationIOMangager/UserInput.java
+++ src/main/java/utils/applicationIOMangager/UserInput.java
@@ -4,6 +4,14 @@ public class UserInput {
     private String[] participants;
     private int attempt;
 
+    public UserInput(String[] participants, int attempt) {
+        this.attempt = attempt;
+        this.participants = participants;
+    }
+
+    public UserInput() {
+    }
+
     public void setParticipants(String[] participants) {
         this.participants = participants;
     }
